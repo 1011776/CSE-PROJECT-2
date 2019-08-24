a9c03d8b20c0a32a46abdcbd61b9c8b688378fd8
Alistair Parkinson (Y11) <1011776@student.ccgs.wa.edu.au>
2019-08-24 12:08:05 +0800

    Work in progress on init.py

--- cgi-bin/init.py
+++ cgi-bin/init.py
@@ -14,7 +14,21 @@ cursor.execute('''DROP TABLE IF EXISTS Movie''')
 print 'create Player table<br />'
 cursor.execute('''CREATE TABLE Player
                     (StudentID INTEGER PRIMARY KEY,
-                    Year INT`,
+                    Year INT,
+                    FirstName VARCHAR(20),
+                    LastName VARCHAR(20))''')
+
+print 'create Campaign table<br />'
+cursor.execute('''CREATE TABLE Campaign
+                    (CampaignID INTEGER PRIMARY KEY,
+                    StudentID,
+                    FirstName VARCHAR(20),
+                    LastName VARCHAR(20))''')
+
+print 'create Character table<br />'
+cursor.execute('''CREATE TABLE Character
+                    (StudentID INTEGER PRIMARY KEY,
+                    Year INT,
                     FirstName VARCHAR(20),
                     LastName VARCHAR(20))''')
 
