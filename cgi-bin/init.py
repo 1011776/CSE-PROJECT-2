eaf1436a950519cc8523f395098f4b5c95f0e3ff
Alistair Parkinson (Y11) <1011776@student.ccgs.wa.edu.au>
2019-09-02 16:26:01 +0800

    Create character, charaterSpell and spell tables

--- cgi-bin/init.py
+++ cgi-bin/init.py
@@ -12,28 +12,74 @@ print 'Drop tables if they exist<br />'
 cursor.execute('''DROP TABLE IF EXISTS Student, Campaign, Character
         , CharacterSpell, Spell, CharacterProficiency, Proficiency
         , CharacterItem, Item, CharacterAblility, Ability''')
-"""
-print 'create Player table<br />'
-cursor.execute('''CREATE TABLE Player
-                    (StudentID INTEGER PRIMARY KEY,
-                    Year INT,
-                    FirstName VARCHAR(20),
-                    LastName VARCHAR(20))''')
+
+print 'create Student table<br />'
+cursor.execute('''CREATE TABLE Student (
+        StudentID       INTEGER PRIMARY KEY,
+        Year            INT,
+        FirstName       VARCHAR(100),
+        LastName        VARCHAR(100)
+        )''')
 
 print 'create Campaign table<br />'
-cursor.execute('''CREATE TABLE Campaign
-                    (CampaignID INTEGER PRIMARY KEY,
-                    StudentID,
-                    FirstName VARCHAR(20),
-                    LastName VARCHAR(20))''')
+cursor.execute('''CREATE TABLE Campaign (
+        CampaignID      INTEGER PRIMARY KEY,
+        StudentIDFK     FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+        FirstName       VARCHAR(100),
+        LastName        VARCHAR(100)
+        )''')
 
 print 'create Character table<br />'
-cursor.execute('''CREATE TABLE Character
-                    (StudentID INTEGER PRIMARY KEY,
-                    Year INT,
-                    FirstName VARCHAR(20),
-                    LastName VARCHAR(20))''')
-"""
+cursor.execute('''CREATE TABLE Character (
+        CharacterID     INTEGER PRIMARY KEY,
+        Name            VARCHAR(100),
+        StudentIDFK     FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+        CampaignIDFK    FOREIGN KEY (CampaignID) REFERENCES Campaign(CampaignID),
+        Class           VARCHAR(100),
+        Background      VARCHAR(100),
+        Race            VARCHAR(100),
+        Alignment       VARCHAR(100),
+        ExpPoints       INTEGER,
+        Inspiration     INTEGER
+        ProfBonus       INTEGER,
+        Gold            INTEGER,
+        HitPointMax     INTEGER,
+        TotalHitDice    INTEGER,
+        PTraits         VARCHAR(1000),
+        Ideals          VARCHAR(1000),
+        Bonds           VARCHAR(1000),
+        Flaws           VARCHAR(1000),
+        OtherInfo       VARCHAR(1000),
+        Strength        INTEGER,
+        Dexterity       INTEGER,
+        Constitution    INTEGER,
+        Intellegence    INTEGER,
+        Wisdom          INTEGER,
+        Charisma        INTEGER,
+        StrSave         INTEGER,
+        WisSave         INTEGER,
+        ChaSave         INTEGER
+        )''')
+
+print 'create CharacterSpell table'
+cursor.execute('''CREATE TABLE CharacterSpell (
+        CharacterID     FOREIGN KEY (CharacterID) REFERENCES Character(CharacterID),
+        SpellID         FOREIGN KEY (SpellID)
+        )''')
+
+print 'create Spell table<br />'
+cursor.execute('''CREATE TABLE Spell (
+        SpellID         INTEGER PRIMARY KEY,
+        Name            VARCHAR(100),
+        Level           INTEGER,
+        Description     VARCHAR(1000)
+        )''')
+
+print 'create CharacterProficiency table<br />'
+cursor.execute('''CREATE TABLE CharacterProficiency
+        CharacterID     FOREIGN KEY (CharacterID) REFERENCES Character(CharacterID),
+        ProficiencyID   FOREIGN KEY (ProficiencyID)
+        )''')
+
 conn.commit()
 cursor.close()
-
