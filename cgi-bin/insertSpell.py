709049418d73b6204a5ba22a6f0ca4339462260e
Alistair Parkinson (Y11) <1011776@student.ccgs.wa.edu.au>
2019-09-09 13:59:25 +0800

    Add print message to spell insertion script

--- cgi-bin/insertSpell.py
+++ cgi-bin/insertSpell.py
@@ -8,6 +8,12 @@ mydb = 'dnd.db'
 conn = sqlite3.connect(mydb)
 cursor = conn.cursor()
 
+print('<head><title>D&D Database: Insert a spell</title></head>')
+
+print('<h2>D&D Database</h2>')
+print('<h3>Insert a spell</h3>')
+
+print('Insert spell<br>')
 form = cgi.FieldStorage()
 name = form.getvalue('name')
 level = form.getvalue('level')
@@ -19,3 +25,8 @@ cursor.execute('''
         INSERT INTO Spell (Name, Level, Description) 
         VALUES (:name, :level, :description)
         ''', values)
+
+print('<br>')
+print('Insertion completed<br>')
+print('<br>')
+print('<a href="../index.html">Return to homepage</a><br>')
