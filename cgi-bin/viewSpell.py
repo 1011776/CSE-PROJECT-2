e3d7c7e2a0dcedc95afb1df3f25d6df21a5a7a00
Alistair Parkinson (Y11) <1011776@student.ccgs.wa.edu.au>
2019-09-12 11:13:23 +0800

    Write view spell script

---
+++ cgi-bin/viewSpell.py
@@ -0,0 +1,46 @@
+print('Content-type: text/html\n\n')
+
+import cgi
+import cgitb; cgitb.enable()
+import sqlite3
+
+mydb = 'dnd.db'
+conn = sqlite3.connect(mydb)
+cursor = conn.cursor()
+
+print('<title>D&D Database: Insert a spell</title>')
+print('<link rel="stylesheet" href="../stylesheet.css">')
+
+print('<h1>D&D Database</h1>')
+print('<h2>View Spell</h2>')
+
+form = cgi.FieldStorage()
+spellID = form.getvalue('spellID')
+
+values = { "spellID": spellID }
+
+cursor.execute('''
+        SELECT SpellID, Name, Level FROM Spell
+        WHERE :spellID = SpellID
+        ''', values)
+
+records = cursor.fetchall()
+
+print("Name: ")
+print(records[0][1])
+
+print('<br/>')
+print('<br/>')
+print('<form action="../index.html">')
+print('<input type=submit value="Return to Homepage"/>')
+print('</form>')
+
+print('<div class="footer">')
+print('D&D Database is unofficial Fan Content permitted under')
+print('the Fan Content Policy. Not approved/endorsed by')
+print('Wizards. Portions of the materials used are property')
+print('of Wizards of the Coast. &#169;Wizards of the Coast LLC')
+print('</div>')
+
+conn.commit()
+cursor.close()
